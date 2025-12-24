0ecdf90ad382b72676e6c58422cf8f97a7ec0a5c
Laxerem <grishaMas@yandex.ru>
2025-12-24 14:31:18 +0500

    [UPDATE]: Add fixed size to context manager

--- Neur.Server.Net.Infrastructure/ContextManager.cs
+++ Neur.Server.Net.Infrastructure/ContextManager.cs
@@ -5,9 +5,34 @@ namespace Neur.Server.Net.Infrastructure;
 
 public class ContextManager {
     private StringBuilder _sb;
+    private readonly int _maxLength;
     
-    public ContextManager() {
+    public ContextManager(int maxLength) {
         _sb = new StringBuilder();
+        _maxLength = maxLength;
+    }
+
+    private string NormalizeMessage(string message) {
+        var normalized = string.Empty;
+        var tokens = message.Split("\n");
+        
+        bool isThink = false;
+
+        foreach (var token in tokens) {
+            if (token == "<think>") {
+                isThink = true;
+            }
+            else if (token == "</think>") {
+                isThink = false;
+                continue;
+            }
+
+            if (!isThink) {
+                normalized += token;
+            }
+        }
+        
+        return normalized.Trim();
     }
 
     public void AddBaseContext(string context) {
@@ -16,9 +41,17 @@ public class ContextManager {
     }
 
     public void AddChatHistory(List<MessageEntity> messages) {
-        _sb.AppendLine("CHAT HISTORY:");
-        for (int i = 0; i < messages.Count; i++) {
-            _sb.AppendLine($"[MESSAGE %{i}] {messages[i].Role.ToString()}: {messages[i].Content}[/MESSAGE]");
+        if (messages.Count > 1) {
+            _sb.AppendLine("CHAT HISTORY:");
+            var messageBuilder = new StringBuilder();
+        
+            for (int i = messages.Count-2; i > 0; i--) {
+                var message = $"[MESSAGE %{i}] {messages[i].Role.ToString()}: {NormalizeMessage(messages[i].Content)}[/MESSAGE]";
+                if ((_sb.Length + message.Length) <= _maxLength) {
+                    messageBuilder.AppendLine(message);
+                }
+            }
+            _sb.AppendLine(messageBuilder.ToString());
         }
     }
 
