834af676765bf4aca42e981ead997698090337b1
Laxerem <grishaMas@yandex.ru>
2026-04-13 15:18:43 +0500

    refactor(service): change an entity saving method

--- Neur.Server.Net.Application/Services/TokenService.cs
+++ Neur.Server.Net.Application/Services/TokenService.cs
@@ -1,40 +1,31 @@
 using System.Security.Claims;
 using Neur.Server.Net.Application.Exeptions;
+using Neur.Server.Net.Application.Interfaces;
+using Neur.Server.Net.Core.Repositories;
 using Neur.Server.Net.Postgres;
 
 namespace Neur.Server.Net.Application.Services;
 
 public class TokenService : ITokenService {
-    private readonly ApplicationDbContext _context;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IUsersRepository _usersRepository;
     
-    public TokenService(ApplicationDbContext context) {
-        _context = context;
+    public TokenService(IUnitOfWork unitOfWork, IUsersRepository usersRepository) {
+        _unitOfWork = unitOfWork;
+        _usersRepository = usersRepository;
     }
 
     public async Task GiveTokens(Guid ownerId, Guid userId, int tokenCount) {
-        var owner = await _context.Users.FindAsync(ownerId);
-        var user = await _context.Users.FindAsync(userId);
-        if (owner == null) {
-            throw new NotFoundException("Owner not found");
-        }
-        if (user == null) {
-            throw new NotFoundException("User not found");
-        }
+        var owner = await _usersRepository.GetByIdAsync(ownerId, true) ?? throw new NotFoundException("Owner not found");
+        var user = await _usersRepository.GetByIdAsync(userId, true) ??  throw new NotFoundException("User not found");
+        
         if (owner.Tokens < tokenCount || tokenCount <= 0) {
             throw new BillingException();
         }
+        
+        owner.Tokens -= tokenCount;
+        user.Tokens += tokenCount;
 
-        await using var transaction = await _context.Database.BeginTransactionAsync();
-        try {
-            owner.Tokens -= tokenCount;
-            user.Tokens += tokenCount;
-
-            await _context.SaveChangesAsync();
-            await transaction.CommitAsync();
-        }
-        catch (Exception e) {
-            await transaction.RollbackAsync();
-            throw;
-        }
+        await _unitOfWork.SaveChangesAsync();
     }
 }
